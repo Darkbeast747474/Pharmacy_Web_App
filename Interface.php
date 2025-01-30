691785e5b952755cf38164c9a1f9f0455dfa2856
AmitSingh511 <sandhuamit4217@gmail.com>
2025-01-30 21:04:36 +0530

    designing navbar bro check it...

--- Interface.php
+++ Interface.php
@@ -15,29 +15,28 @@
         <ul class="nav_list">
             <div>
                 <li><a href="#" onclick="load('Dashboard.php')"><i class="fas fa-tv"></i> Dashboard</a></li>
-                <li><a href="#" onclick="load()"><i class="fa-solid fa-magnifying-glass"></i>Invoice search</a></li>
-                <li><a href="#" onclick="load()"><i class="fa-solid fa-warehouse"></i>Medicine Inventory</a></li>
+                <li><a href="#" onclick="load()"><i class="fa-solid fa-magnifying-glass"></i>InvoiceSearch</a></li>
+                <li><a href="#" onclick="load()"><i class="fa-solid fa-warehouse"></i>MedicineInventory</a></li>
             </div><br>
-            <div>
+            <!-- <div>
                 <p>Pharmacy Company</p>
                 <li><a href="#" onclick="load()"><i class="fa-solid fa-plus"></i>Add company</a></li>
                 <li><a href="#" onclick="load()"><i class="fa-solid fa-briefcase"></i>Manage Company</a></li>
-            </div><br>
+            </div><br> -->
             <div>
                 <p>Medicine</p>
-                <li><a href="#" onclick="load('manage_medications.php')"><i class="fa-solid fa-pills"></i>Manage
-                        Medicine</a></li>
+                <li><a href="#" onclick="load('manage_medications.php')"><i class="fa-solid fa-pills"></i>ManageMedicine</a></li>
             </div><br>
             <div>
                 <p>Pharmacist</p>
-                <li><a href="#" onclick="load()"><i class="fa-solid fa-user"></i>Add Pharmacist</a></li>
-                <li><a href="#" onclick="load()"><i class="fa-solid fa-briefcase"></i>Manage Pharmacist</a></li>
+                <li><a href="#" onclick="load()"><i class="fa-solid fa-user"></i>AddPharmacist</a></li>
+                <li><a href="#" onclick="load()"><i class="fa-solid fa-briefcase"></i>ManagePharmacist</a></li>
             </div><br>
             <div>
                 <p>Reports</p>
-                <li><a href="#" onclick="load()"><i class="fa-solid fa-chart-simple"></i>Stack Reports</a></li>
-                <li><a href="#" onclick="load()"><i class="fa-solid fa-palette"></i>Pharmacist Wise Reports </a></li>
-                <li><a href="#" onclick="load()"><i class="fa-solid fa-receipt"></i>Sales Reports</a></li>
+                <li><a href="#" onclick="load()"><i class="fa-solid fa-chart-simple"></i>StackReports</a></li>
+                <!-- <li><a href="#" onclick="load()"><i class="fa-solid fa-palette"></i>Pharmacist Wise Reports </a></li> -->
+                <li><a href="#" onclick="load()"><i class="fa-solid fa-receipt"></i>SalesReports</a></li>
             </div>
         </ul>
     </nav>
